c674785c44c6bf2498829b97340636bb08c4f09e
Your Name <you@example.com>
2025-07-26 16:47:49 +0530

    Build VinaySchool HomePage

---
+++ SpringProject_20/src/main/java/com/vinaysteja/vinayschool/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.vinaysteja.vinayschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value={"", "/", "home"})
+    public String displayHomePage() {
+        
+        return "home.html";
+    }
+
+}
