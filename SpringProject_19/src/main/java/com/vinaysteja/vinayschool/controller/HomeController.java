afaa803e3b54a258c7a023c44a47cbd3dc5acfd2
Your Name <you@example.com>
2025-07-26 14:50:33 +0530

    Vinay school Web App using Thymeleaf & DevTools Demo

---
+++ SpringProject_19/src/main/java/com/vinaysteja/vinayschool/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.vinaysteja.vinayschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value={"", "/", "home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "John Doe");
+        return "home.html";
+    }
+
+}
