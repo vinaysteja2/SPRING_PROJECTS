942f78d42fcae9fcca5f298dc8a207d1067d3958
Your Name <you@example.com>
2025-07-27 13:01:52 +0530

    Validating the form input using Java Bean & Hibernate Validators

---
+++ SpringProject_24/src/test/java/com/vinaysteja/vinayschool/VinayschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinaysteja.vinayschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinayschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
