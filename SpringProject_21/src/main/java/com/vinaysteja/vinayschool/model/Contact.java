6926d48a6532b2082739c78579a9df4b9fe3a6ae
Your Name <you@example.com>
2025-07-27 07:22:53 +0530

    Build VinaySchool Courses, About, Contact Pages & Intro to Lombok

---
+++ SpringProject_21/src/main/java/com/vinaysteja/vinayschool/model/Contact.java
@@ -0,0 +1,14 @@
+package com.vinaysteja.vinayschool.model;
+
+import lombok.Data;
+
+@Data
+public class Contact {
+
+    private String name;
+    private String mobileNum;
+    private String email;
+    private String subject;
+    private String message;
+	
+}
