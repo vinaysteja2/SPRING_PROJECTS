5c368a0a1fcbad741d68e3d5ac533afe179f0a68
Your Name <you@example.com>
2025-09-04 13:23:39 +0530

    Migrate from JDBCTemplate to Spring Data JPA

---
+++ SpringProject_35/src/main/java/com/vinaysteja/vinayschool/VinayschoolApplication.java
@@ -0,0 +1,20 @@
+package com.vinaysteja.vinayschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+//@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
+@EnableJpaRepositories("com.vinaysteja.vinayschool.repository")
+@EntityScan("com.vinaysteja.vinayschool.model")
+public class VinayschoolApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(VinayschoolApplication.class, args);
+    }
+
+}
