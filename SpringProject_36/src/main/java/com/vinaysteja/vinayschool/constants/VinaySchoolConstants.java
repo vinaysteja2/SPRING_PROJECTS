5cfa120137af66e16529f8d6ddeecb062bd7ab00
Your Name <you@example.com>
2025-09-06 06:28:17 +0530

    Spring JPA Auditing using CreatedBy,LastModifiedBy,CreatedDate,LastModifiedDate

---
+++ SpringProject_36/src/main/java/com/vinaysteja/vinayschool/constants/VinaySchoolConstants.java
@@ -0,0 +1,11 @@
+package com.vinaysteja.vinayschool.constants;
+
+public interface VinaySchoolConstants {
+
+
+
+        public static final String OPEN = "Open";
+        public static final String CLOSE = "Close";
+
+
+}
---
+++ SpringProject_36/src/main/java/com/vinaysteja/vinayschool/service/ContactService.java
@@ -0,0 +1,70 @@
+package com.vinaysteja.vinayschool.service;
+
+
+import com.vinaysteja.vinayschool.constants.VinaySchoolConstants;
+import com.vinaysteja.vinayschool.repository.ContactRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
+
+import com.vinaysteja.vinayschool.controller.ContactController;
+import com.vinaysteja.vinayschool.model.Contact;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+
+/*
+@Slf4j, is a Lombok-provided annotation that will automatically generate an SLF4J
+Logger static property in the class at compilation time.
+* */
+@Slf4j
+@Service
+public class ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    /**
+     * Save Contact Details into DB
+     * @param contact
+     * @return boolean
+     */
+    public boolean saveMessageDetails(Contact contact){
+        boolean isSaved = false;
+        contact.setStatus(VinaySchoolConstants.OPEN);
+        Contact savedContact = contactRepository.save(contact);
+        if(null != savedContact && savedContact.getContactId()>0) {
+            isSaved = true;
+        }
+        return isSaved;
+    }
+
+    public List<Contact> findMsgsWithOpenStatus(){
+        List<Contact> contactMsgs = contactRepository.findByStatus(VinaySchoolConstants.OPEN);
+        return contactMsgs;
+    }
+
+    public boolean updateMsgStatus(int contactId){
+        boolean isUpdated = false;
+        Optional<Contact> contact = contactRepository.findById(contactId);
+        contact.ifPresent(contact1 -> {
+            contact1.setStatus(VinaySchoolConstants.CLOSE);
+        });
+        Contact updatedContact = contactRepository.save(contact.get());
+        if(null != updatedContact && updatedContact.getUpdatedBy()!=null) {
+            isUpdated = true;
+        }
+
+        return isUpdated;
+
+    }
+
+}
