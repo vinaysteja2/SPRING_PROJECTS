0d560fc7462d2408b7b41c05328c2644b65c5306
Your Name <you@example.com>
2025-09-10 15:36:55 +0530

    Authentication with credentials in DB using JPA

---
+++ SpringProject_38/src/main/java/com/vinaysteja/vinayschool/annotation/PasswordValidator.java
@@ -0,0 +1,18 @@
+package com.vinaysteja.vinayschool.annotation;
+
+
+
+import com.vinaysteja.vinayschool.validations.PasswordStrengthValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordStrengthValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PasswordValidator {
+    String message() default "Please choose a strong password";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ SpringProject_38/src/main/java/com/vinaysteja/vinayschool/repository/HolidaysRepository.java
@@ -0,0 +1,28 @@
+package com.vinaysteja.vinayschool.repository;
+
+
+import com.vinaysteja.vinayschool.model.Holiday;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/*
+@Repository stereotype annotation is used to add a bean of this class
+type to the Spring context and indicate that given Bean is used to perform
+DB related operations and
+* */
+@Repository
+public interface HolidaysRepository extends CrudRepository<Holiday, String> {
+
+
+}
+
